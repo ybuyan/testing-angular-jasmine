81bcb83cc5776673e66762f25a4dffbf3643928f
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2019-11-27 23:47:33 +0000

    Testing `EventEmiiter`

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,11 +1,24 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
+
   let component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {});
+  it('Should raise `voteChanged` event when upvoted', () => {
+
+    let totalVotes: number = null;
+
+    component.voteChanged.subscribe((newTotal: number) => {
+      totalVotes = newTotal;
+    });
+
+    component.upVote();
+
+    expect(totalVotes).toBe(1);
+  });
+
 });
