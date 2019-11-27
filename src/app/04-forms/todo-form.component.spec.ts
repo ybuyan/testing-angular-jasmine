125da924a2c3f039299abe4addc580896d7e60a0
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2019-11-27 23:29:36 +0000

    Testing the form builder and the validators (`required`)

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,11 +1,24 @@
+import { FormBuilder } from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
+
   let component: TodoFormComponent;
 
-  beforeEach(() => {});
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder());
+  });
 
-  it('', () => {});
+  it('Should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  });
 
-  it('', () => {});
+  it('Should make the `name` control required', () => {
+    const formControl = component.form.get('name');
+    formControl.setValue('');
+
+    expect(formControl.valid).toBeFalsy();
+  });
+
 });
