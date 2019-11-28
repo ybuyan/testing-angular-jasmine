b7cc32e7347b3b5cf2df1862a228f0f9be55862e
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2019-11-28 09:51:53 +0000

    Using `spyOn` to test service return creating a fake Observable

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,10 +1,27 @@
-import { TodosComponent } from './todos.component';
+import { from } from 'rxjs/internal/observable/from';
 import { TodoService } from './todo.service';
+import { TodosComponent } from './todos.component';
 
 describe('TodosComponent', () => {
+
   let component: TodosComponent;
+  let service: TodoService;
 
-  beforeEach(() => {});
+  beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
+  });
 
-  it('', () => {});
+  it('Should set `todos` property with items returned form the server', () => {
+
+    const toDos = [1, 2, 3];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return from([toDos]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos).toBe(toDos);
+  });
 });
--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -1,24 +1,37 @@
-import { TodoService } from './todo.service';
 import { OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 export class TodosComponent implements OnInit {
-  todos: any = [];
-  message;
 
-  constructor(private service: TodoService) {}
+  todos: any = [];
+  message: any;
+
+  constructor(
+    private service: TodoService,
+  ) { }
 
   ngOnInit() {
-    this.service.getTodos().subscribe(t => (this.todos = t));
+    this.service
+      .getTodos()
+      .subscribe((toDos) => this.todos = toDos);
   }
 
-  add() {
+  add(): void {
     const newTodo = { title: '... ' };
-    this.service.add(newTodo).subscribe(t => this.todos.push(t), err => (this.message = err));
+
+    this.service
+      .add(newTodo)
+      .subscribe(
+        (toDo) => this.todos.push(toDo),
+        (error: any) => this.message = error
+      );
   }
 
-  delete(id) {
+  delete(id: number): void {
     if (confirm('Are you sure?')) {
-      this.service.delete(id).subscribe();
+      this.service
+        .delete(id)
+        .subscribe();
     }
   }
 }
