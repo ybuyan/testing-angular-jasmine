2d32dcd7e009c792ed111d9e256d4611b7146f9d
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2019-12-08 17:41:07 +0000

    Adding the dependency injection of the `TodoService`

--- src/app/08-integration-tests/2-todos/todo.service.ts
+++ src/app/08-integration-tests/2-todos/todo.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/operators';
 
+@Injectable()
 export class TodoService {
 
   constructor(
--- src/app/08-integration-tests/2-todos/todos.component.spec.ts
+++ src/app/08-integration-tests/2-todos/todos.component.spec.ts
@@ -1,12 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TodoService } from './todo.service';
 import { TodosComponent } from './todos.component';
 
-/**
- * NOTE: I've deliberately excluded this suite from running because the test will fail. This is because we have not
- * provided the TodoService as a dependency to TodosComponent. When you get to Lecture 6 (Providing Dependencies), be
- * sure to remove "x" from "xdescribe" below
- */
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
 
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
@@ -14,7 +11,18 @@ xdescribe('TodosComponent', () => {
   beforeEach(async(() => {
     TestBed
       .configureTestingModule({
-        declarations: [TodosComponent]
+
+        declarations: [
+          TodosComponent,
+        ],
+
+        imports: [
+          HttpClientModule,
+        ],
+
+        providers: [
+          TodoService,
+        ]
       })
       .compileComponents();
   }));
